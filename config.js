e55dc06456919a49a72dd66413de10d290fe47fb
Mark Moffat <hi@markmoffat.com>
2021-03-29 20:01:03 +1030

    init

---
+++ config.js
@@ -0,0 +1,15 @@
+const blog = {
+    name: 'mrvautin',
+    description: 'This is the blog description',
+    twitterHandle: '@mrvautin',
+    baseUrl: 'http://localhost:4965',
+    layout: 'layout.hbs',
+    sourceDir: 'posts/',
+    sourcesExt: 'markdown',
+    summaryLength: 250,
+    port: 4965
+};
+
+module.exports = {
+    blog
+};
