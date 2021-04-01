77577920b775a124d292a537505f27295b2dbe85
Mark Moffat <hi@markmoffat.com>
2021-04-01 16:42:09 +1030

    Stopped tracking build files

--- build/content/javascripts/site.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var images = document.getElementsByTagName("img");
-    for (var i = 0; i < images.length; i++) {
-        images[i].classList.add('img-fluid');
-      }
-}, false);
