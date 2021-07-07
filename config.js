40b984b4f5af9ce672211daed2a91371c0588bf9
Mark Moffat <hi@markmoffat.com>
2021-07-07 18:15:43 +0930

    Added postBuild by default to example

--- config.js
+++ config.js
@@ -8,7 +8,13 @@ const config = {
         summaryLength: 250,
         port: 4965,
         pagination: true,
-        postPerPage: 8
+        postPerPage: 8,
+        postBuild: [
+            {
+                name: 'zip',
+                options: {}
+            }
+        ]
     },
     production: {
         name: 'squido',
